081b952a5c8663ee8913a98ce1e47d0446b1b546
Charlie Clark <cclark@squarespace.com>
2022-12-07 12:49:05 -0500

    day 6

---
+++ src/days/6/1.js
@@ -0,0 +1,21 @@
+module.exports = ([code]) => {
+  const chars = code.split("");
+
+  let prevChars = [];
+
+  for (let i = 0; i < chars.length; i++) {
+    const char = chars[i];
+
+    const matchedCharIndex = prevChars.indexOf(char);
+
+    if (matchedCharIndex !== -1) {
+      prevChars = prevChars.slice(matchedCharIndex + 1);
+    }
+
+    prevChars.push(char);
+
+    if (prevChars.length === 4) {
+      return i + 1;
+    }
+  }
+};
