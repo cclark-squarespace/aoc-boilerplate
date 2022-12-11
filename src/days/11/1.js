68e03be3c1f5d2dd19176c9b39f7cb4464448b1e
Charlie Clark <cclark@squarespace.com>
2022-12-11 17:03:33 -0500

    day 11

---
+++ src/days/11/1.js
@@ -0,0 +1,87 @@
+const stepCache = {};
+
+module.exports = (lines, raw) => {
+  const steps = raw.split(/\n\s*\n/);
+
+  const monkeyItems = [];
+  const monkeyScore = [];
+
+  const command = (operator, num) => (old) => {
+    const useNum = num === "old" ? old : Number(num);
+
+    if (operator === "+") {
+      return old + useNum;
+    } else if (operator === "*") {
+      return old * useNum;
+    }
+  };
+
+  const parseStep = (step) => {
+    if (stepCache[step]) {
+      return stepCache[step];
+    }
+
+    const [l1, l2, l3, l4, l5, l6] = step
+      .split("\n")
+      .map((s) => s.split(":").map((s) => s.trim()));
+
+    const monkeyIndex = Number(l1[0].split(" ")[1]);
+
+    const startingItems = l2[1]
+      .split(",")
+      .filter((s) => !!s)
+      .map((s) => Number(s.trim()));
+
+    const monkeyCommand = (() => {
+      const [, operator, num] = l3[1].match(/new = old ([+*]) (.+)/);
+      return command(operator, num);
+    })();
+
+    const divisibleBy = Number(l4[1].replace("divisible by ", ""));
+    const trueIndex = Number(l5[1].replace("throw to monkey ", ""));
+    const falseIndex = Number(l6[1].replace("throw to monkey ", ""));
+
+    const data = {
+      monkeyIndex,
+      startingItems,
+      monkeyCommand,
+      divisibleBy,
+      trueIndex,
+      falseIndex,
+    };
+
+    stepCache[step] = data;
+
+    return data;
+  };
+
+  steps.forEach((step) => {
+    const { monkeyIndex, startingItems } = parseStep(step);
+    monkeyItems[monkeyIndex] = startingItems;
+    monkeyScore[monkeyIndex] = 0;
+  });
+
+  for (let i = 0; i < 20; i++) {
+    steps.forEach((step) => {
+      const { monkeyIndex, monkeyCommand, divisibleBy, trueIndex, falseIndex } =
+        parseStep(step);
+
+      while (monkeyItems[monkeyIndex].length > 0) {
+        const itemToInspect = monkeyItems[monkeyIndex].shift();
+        const newWorryLevel = Math.floor(monkeyCommand(itemToInspect) / 3);
+
+        monkeyScore[monkeyIndex]++;
+
+        if (newWorryLevel % divisibleBy === 0) {
+          monkeyItems[trueIndex].push(newWorryLevel);
+        } else {
+          monkeyItems[falseIndex].push(newWorryLevel);
+        }
+      }
+    });
+  }
+
+  monkeyScore.sort((a, b) => b - a);
+
+  return monkeyScore[0] * monkeyScore[1];
+};
--- src/index.js
+++ src/index.js
@@ -8,15 +8,16 @@ const getLines = (day) => {
     `${__dirname}/days/${day}/input.txt`,
     "utf-8"
   );
-  const lines = rawInput.trim().split("\n");
-  return lines;
+  const raw = rawInput.trim();
+  const lines = raw.split("\n");
+  return { lines, raw };
 };
 
 const run = () => {
   const { day, star } = argv;
-  const lines = getLines(day);
+  const { lines, raw } = getLines(day);
   const runCode = require(`${__dirname}/days/${day}/${star}.js`);
-  console.info(runCode(lines));
+  console.info(runCode(lines, raw));
 };
 
 run();
